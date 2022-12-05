87e9d9ed632f3dc3955b09b39f027e262bdf30d6
UlinoyaPed <l15010104769@outlook.com>
2022-12-05 11:00:38 +0800

    dl ubn

---
+++ HttpXiazai.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+mkdir ./download
+cd ./download
+wget -q https://releases.ubuntu.com/22.04.1/ubuntu-22.04.1-desktop-amd64.iso
+echo ====folder $(pwd) ====
+ls
+fname=$(ls *.*)
+echo ====name of download file====
+echo $fname
+echo ====split file====
+split -b1999M --verbose $fname $fname\_
+echo ====after split====
+ls
+rm -f $fname
+echo ====after remove original file====
+ls
