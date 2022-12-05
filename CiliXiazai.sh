4f65c9048b5c379b00ff4d1ccf0afdb499f249f9
UlinoyaPed <l15010104769@outlook.com>
2022-12-05 17:16:50 +0800

    dl xhr

---
+++ CiliXiazai.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+rootdir=$(pwd)
+
+mkdir $rootdir/download
+mkdir $rootdir/cache
+cd $rootdir/cache
+
+npm install -g t-get
+
+tget 'magnet:?xt=urn:btih:35D760008EC922B341A286A0B3DBCD08A2488D99'
+
+echo ====folder $(pwd)====
+ls
+
+cd $(ls)
+echo ====folder $(pwd)====
+ls
+
+fname=$(find . -size +2000M)
+echo ====fname====
+echo $fname
+echo ====split file====
+split -b2000M --verbose $fname $fname\_
+echo ====after split====
+ls
+rm -f $fname
+echo ====after remove original file====
+ls
+
+mv ./* $rootdir/download/
+
+IFS=$SAVEIFS
