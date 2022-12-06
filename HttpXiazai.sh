bdb78239d6031472af60dd9297f9f38bc4b75cd1
UlinoyaPed <l15010104769@outlook.com>
2022-12-06 14:14:17 +0800

    dl xhr

--- HttpXiazai.sh
+++ HttpXiazai.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 rootdir=$(pwd)
+
 mkdir ./download
 cd ./download
+
 wget -q https://releases.ubuntu.com/22.04.1/ubuntu-22.04.1-desktop-amd64.iso
+
 echo ====folder $(pwd) ====
 ls
-fname=$(ls *.*)
+
+fname=$(find . -size +2000M)
 echo ====name of download file====
 echo $fname
 echo ====split file====
